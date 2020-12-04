ac109fb1f09738cfb88874afc6e99a16d90ee8b0
nicoulmete1 <nicoulmete1@gmail.com>
2020-12-04 11:29:45 -0300

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,40 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useMemo } from 'react';
-import { useFetch } from "./useFetch";
+import { useReducer, useState } from 'react';
 
-const computeLongestWord = (arr) => {
-  if (!arr) {
-    return [];
-  }
-
-  console.log('computing longest word');
-
-  let longestWord = '';
-
-  arr.forEach(sentence => {
-    sentence.split(' ').forEach(word => {
-      if (word.length > longestWord.length) {
-        longestWord = word;
+function reducer(state, action) {
+  // state: current value
+  // action: function called - param received from dispatch(param)
+  switch (action.type) {
+    // case 'increment':
+      // do not use 'state++' because it mutates the state
+    //   return state + 1;
+    // case 'decrement':
+    //   return state - 1;
+    case 'add-todo':
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+        todoCount: state.todoCount + 1
       }
-    });
-  });
-
-  return longestWord;
+    case 'toggle-todo':
+      return {
+        todos: state.todos.map((t, idx) => idx === action.idx ? { ...t, completed: !t.completed } : t),
+        todoCount: state.todoCount
+      }
+    default:
+      return state;
+  }
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-  const { data } = useFetch('https://raw.githubusercontent.com/ajzbc/kanye.rest/quotes/quotes.json');
+  // const [count, dispatch] = useReducer(reducer, 0);
 
-  const longestWord = useMemo(() => computeLongestWord(data), [data]);
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, { todos: [], todoCount: 0 });
+  const [text, setText] = useState();
 
   return (
     <div>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(count + 1)}>increment</button>
-      {/* <div>{computeLongestWord(data)}</div> */}
-      <div>{longestWord}</div>
+      {/* <div>count: {count}</div>
+      <button onClick={() => dispatch({ type: 'increment' })}>increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>decrement</button> */}
+
+      <form onSubmit={e => {
+        e.preventDefault();
+        dispatch({ type: 'add-todo', text });
+        setText('');
+      }}>
+        <input type="text" value={text || ''} onChange={e => setText(e.target.value)} />
+      </form>
+
+      {/* <pre>
+        {JSON.stringify(todos, null, 2)}
+      </pre> */}
+
+      <div>number of todos: {todoCount}</div>
+
+      {todos.map((t, idx) => (
+        <div
+          key={t.text}
+          onClick={() => dispatch({ type: 'toggle-todo', idx })}
+          style={{
+            textDecoration: t.completed ? 'line-through' : ''
+          }}
+        >{t.text}</div>
+      ))}
     </div>
   );
 }
