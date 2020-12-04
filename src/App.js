4eac2d1d2ef1a5b10bb5abeefc2778974b790260
nicoulmete1 <nicoulmete1@gmail.com>
2020-12-04 10:31:42 -0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,97 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect, useRef } from 'react';
-import { useForm } from './useForm';
 import { Hello } from './Hello';
-import { useFetch } from './useFetch';
+import { useCallback, useState } from 'react';
+import { Square } from './Square';
 
 function App() {
-  // useState - Normal
-  // const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
+  const favoriteNums = [7, 21, 37];
 
-  // useState - Object
-  // const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
-
-  // useState - Split
-  // const [count, setCount] = useState(10);
-  // const [count2, setCount2] = useState(20);
-
-  // useState - Form
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  const [values, handleChange] = useForm({ email: '', password: '', firstName: '' });
-
-  // useEffect
-  // useEffect(() => {
-  //   console.log('render');
-
-  //   // cleanup function (unmount)
-  //   return () => {
-  //     console.log('unmount');
-  //   }
-  // }, [values.email]);
-
-  // useEffect with child component
-  const [showHello, setShowHello] = useState(true);
-
-  // useEffect + useState to fetch from API
-  // const [count, setCount] = useState(JSON.parse(window.localStorage.getItem('count'))); called everytime
-  // const [count, setCount] = useState(() => {
-  //   return JSON.parse(localStorage.getItem('count')) || 0;
-  // });
-  // const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-
-  // everytime count changes, save it to localStorage
-  // useEffect(() => {
-  //   localStorage.setItem('count', JSON.stringify(count));
-  // }, [count]);
-
-  // useRef
-  const inputRef = useRef();
+  // when count or setCount change, increment fn is recreated
+  // const increment = useCallback(() => {
+  //   setCount(count + 1);
+  // }, [count, setCount]);
+  
+  const increment = useCallback((n) => {
+    setCount(c => c + n);
+  }, [setCount]);
 
   return (
-    <div className="App">
-      {/* Normal */}
-      {/* <button onClick={() => setCount(count + 1)}>+</button> */}
-      {/* <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button> */}
+    <div>
+      {/* this lambda is created on every single render */}
+      <Hello increment={increment} />
+      <div>count: {count}</div>
 
-      {/* Object */}
-      {/* <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))}>+</button> */}
+      {favoriteNums.map(n => {
+        return (
+          // <Square onClick={() => increment(n)} n={n} key={n} />
 
-      {/* Split */}
-      {/* <button
-        onClick={() => {
-          setCount(c => c + 1);
-          setCount2(c => c + 1);
-        }}
-      >
-        +
-      </button>
-      <div>count 1: {count}</div>
-      <div>count 2: {count2}</div> */}
-
-      {/* Form */}
-      {/* <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-      <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} /> */}
-      
-      {/* useEffect with child component */}
-      <button onClick={() => setShowHello(!showHello)}>toggle hello</button>
-      {showHello && <Hello />}
-
-      {/* custom useEffect + useState */}
-      {/* <div>
-        <div>{!data ? 'loading...' : data}</div>
-        <div>count: {count}</div>
-        <button onClick={() => setCount(c => c + 1)}>increment</button>
-      </div> */}
-
-      <input ref={inputRef} type="email" placeholder="email" name="email" value={values.email} onChange={handleChange} />
-      <input type="text" placeholder="first name" name="firstName" value={values.firstName} onChange={handleChange} />
-      <input type="password" placeholder="password" name="password" value={values.password} onChange={handleChange} />
-
-      <button onClick={() => {
-        inputRef.current.focus();
-      }}>focus</button>
+          // prevent re-rendering moving increment logic to Square component
+          <Square increment={increment} n={n} key={n} />
+        );
+      })}
     </div>
   );
 }
--- src/Hello.js
+++ src/Hello.js
@@ -1,26 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useFetch } from "./useFetch";
+import React, { useRef } from "react";
+import { useCountRenders } from "./useCountRenders";
 
-export const Hello = () => {
-  // useEffect + useState to fetch from API
-  // const [count, setCount] = useState(JSON.parse(window.localStorage.getItem('count'))); called everytime
-  const [count, setCount] = useState(() => {
-    return JSON.parse(localStorage.getItem('count')) || 0;
-  });
-  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-
-  // everytime count changes, save it to localStorage
-  useEffect(() => {
-    localStorage.setItem('count', JSON.stringify(count));
-  }, [count]);
+export const Hello = React.memo(({ increment }) => {
+  useCountRenders();
 
   return (
-    <div>
-      <div>
-        <div>{!data ? 'loading...' : data}</div>
-        <div>count: {count}</div>
-        <button onClick={() => setCount(c => c + 1)}>increment</button>
-      </div>
-    </div>
+    <button onClick={() => increment(5)}>
+      hello
+    </button>
   );
-}
+});
